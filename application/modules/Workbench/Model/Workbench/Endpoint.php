3be6167cf0eb6345780d2e6b7a9c5786012fe530
Richard Tuin <richardtuin@gmail.com>
2012-02-27 12:30:05 +0100

    Using Glitch_Registry::get directly instead of getInstance

--- application/modules/Workbench/Model/Workbench/Endpoint.php
+++ application/modules/Workbench/Model/Workbench/Endpoint.php
@@ -127,8 +127,8 @@ class Workbench_Model_Workbench_Endpoint
     {
         $this->_method = strtolower($method);
         //@todo: remove me!
-        $settings = Glitch_Registry::getInstance(Glitch_Registry::KEY_SETTINGS);
-        if (!$settings->settings->workbench->enablecommit && 'get' !== $this->_method) {
+        $settings = Glitch_Registry::get(Glitch_Registry::KEY_SETTINGS);
+        if (!$settings->workbench->enablecommit && 'get' !== $this->_method) {
             $this->setDisableCommit(true);
         }
         return $this;
